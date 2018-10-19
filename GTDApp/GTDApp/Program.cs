79c0eae5cc06d8ddc3475b926678f7a2b007e693
Ferencz Dávid Tamás <fdt0712@gmail.com>
2018-10-19 21:24:38 +0200

    StyleCop setup with custom oenik ruleset

--- GTDApp/GTDApp/Program.cs
+++ GTDApp/GTDApp/Program.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <summary>
+// Program
+// </summary>
+// <copyright file="Program.cs" company="OENIK_PROG3_2018_2_EP7D0O">
+// Copyright © OENIK_PROG3_2018_2_EP7D0O All rights reserved.
+// </copyright>
 
 namespace GTDApp
 {
-    class Program
+    /// <summary>
+    ///     My Main program
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///     My Main program
+        /// </summary>
+        /// <param name="args">Arguments array of strings</param>
+        public static void Main(string[] args)
         {
         }
     }
