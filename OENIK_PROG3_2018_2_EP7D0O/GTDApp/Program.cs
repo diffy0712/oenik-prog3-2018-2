b35fafc5ffaad0d5346caae407bf6a0e1175251a
Ferencz Dávid Tamás <fdt0712@gmail.com>
2018-11-02 23:44:07 +0100

    menu

--- OENIK_PROG3_2018_2_EP7D0O/GTDApp/Program.cs
+++ OENIK_PROG3_2018_2_EP7D0O/GTDApp/Program.cs
@@ -19,7 +19,29 @@ namespace OENIK_PROG3_2018_2_EP7D0O
         /// </summary>
         public static void Main()
         {
-            Console.WriteLine("ok");
+            Console.WriteLine("Hello! This is a GTD app.");
+            Console.WriteLine("Please select a menu item:");
+            Console.WriteLine("lc: List Containers");
+            Console.WriteLine("ac: Add Container");
+            Console.WriteLine("rc: Remove Container");
+            Console.WriteLine("mc: Modify Container");
+            Console.WriteLine("li: List Items");
+            Console.WriteLine("ai: Add Item");
+            Console.WriteLine("ri: Remove Item");
+            Console.WriteLine("mi: Modify Item");
+            Console.WriteLine("r: Review");
+            Console.WriteLine("r: Statistics");
+            Console.WriteLine("javaweb: Java/web interaction");
+
+            string menuSelect = Console.ReadLine();
+
+            switch (menuSelect)
+            {
+                default:
+                    Console.WriteLine("This is not ready");
+                    break;
+            }
+
             Console.ReadKey();
         }
     }
