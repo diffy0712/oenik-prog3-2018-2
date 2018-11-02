4f9b1cc71d059292a9370c21007dc895f523fcec
Ferencz Dávid Tamás <fdt0712@gmail.com>
2018-11-02 20:56:40 +0100

    Create the project structure within the git repository NuGet/Project settings

---
+++ OENIK_PROG3_2018_2_EP7D0O/GTDApp.Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿// <summary>
+// GTD(getting things done) Application
+// </summary>
+// <copyright file="Program.cs" company="OENIK_PROG3_2018_2_EP7D0O">
+// Copyright © OENIK_PROG3_2018_2_EP7D0O All rights reserved.
+// </copyright>
+
+namespace GTDApp.Data
+{
+    public class Class1
+    {
+    }
+}
