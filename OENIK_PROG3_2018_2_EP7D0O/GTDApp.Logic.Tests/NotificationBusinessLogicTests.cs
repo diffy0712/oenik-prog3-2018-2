4c995af707c5a594a255bed18fadefb5d874e1b1
Ferencz Dávid Tamás <fdt0712@gmail.com>
2019-01-02 23:28:42 +0100

    basic notification tests

---
+++ OENIK_PROG3_2018_2_EP7D0O/GTDApp.Logic.Tests/NotificationBusinessLogicTests.cs
@@ -0,0 +1,52 @@
+﻿// <summary>
+// GTD(getting things done) Application
+// </summary>
+// <copyright file="NotificationBusinessLogicTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GtdApp.Logic.Tests
+{
+    using System;
+    using System.Linq;
+    using GtdApp.Data;
+    using Moq;
+    using NUnit.Framework;
+
+    /// <summary>
+    ///      NotificationBusinessLogicTests
+    /// </summary>
+    public class NotificationBusinessLogicTests : BusinessLogicTests
+    {
+        /// <summary>
+        ///     Test the save method's calling for insertion (notification_id is not declared)
+        /// </summary>
+        [Test]
+        public void DoesSaveNotificationAddsNewNotification()
+        {
+            Notification testNewNotification = new Notification()
+            {
+                name = $"Test Notification#01",
+                type = "TestType",
+                amount = 10,
+                unit = "day",
+                updated_at = default(DateTime),
+                created_at = default(DateTime)
+            };
+
+            this.BusinessLogic.SaveNotification(testNewNotification);
+            this.MockNotification.Verify(x => x.Add(testNewNotification), Times.Once);
+        }
+
+        /// <summary>
+        ///     Test the delete method's calling for deletion
+        /// </summary>
+        [Test]
+        public void DoesRemoveNotificationRemovesNotification()
+        {
+            Notification testNotification = this.MockNotification.Object.GetAll().First();
+            this.BusinessLogic.RemoveNotification(testNotification);
+            this.MockNotification.Verify(x => x.Remove(testNotification), Times.Once);
+        }
+    }
+}
